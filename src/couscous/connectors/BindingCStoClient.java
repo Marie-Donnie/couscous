e9e709f2dfea86f3e2715017b2fcc2e2e52b701a
Licherm <montalvo@orange.fr>
2017-12-10 00:22:10 +0100

    Second binding

--- src/couscous/connectors/BindingCStoClient.java
+++ src/couscous/connectors/BindingCStoClient.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import couscous.observable.AbstractConnector;
 
 public class BindingCStoClient extends AbstractConnector {
-
+	
+	// Rapelle à l'inverse des Composants les fournis sont à gauche et les requis à droite
 	public BindingCStoClient() {
 		super();
 		this.addManageable("PortAskClient", "PortReceiveAsk"); // Voir M1
---
+++ src/couscous/connectors/BindingTransmitRequest.java
@@ -0,0 +1,14 @@
+package couscous.connectors;
+
+import couscous.observable.AbstractConnector;
+
+public class BindingTransmitRequest extends AbstractConnector {
+	
+	// Rapelle à l'inverse des Composants les fournis sont à gauche et les requis à droite
+	public BindingTransmitRequest() {
+		super();
+		this.addManageable("PortTransmitRequest", "PortReceiveRequestIntoDetail"); // Pas dans le M1
+		this.addManageable("PortReponseFromDetail", "PortReceiveResponse");
+	}
+
+}
