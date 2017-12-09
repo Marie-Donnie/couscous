55ea40d2680a401e98485275ddf3c6076eb5341c
Licherm <montalvo@orange.fr>
2017-12-09 20:28:40 +0100

    manageable GestionnaireSecurite

--- src/couscous/composants/GestionnaireSecurite.java
+++ src/couscous/composants/GestionnaireSecurite.java
@@ -6,7 +6,11 @@ import couscous.observable.AbstractComposant;
 public class GestionnaireSecurite extends AbstractComposant  {
 
 
-
+	public GestionnaireSecurite () {
+		super();
+		this.addManageable("askAthorization"); // Voir 
+		this.addManageable("dataReady");
+	}
 	@Override
 	public void recevoirMessage(Message msg) {
 		// TODO Auto-generated method stub
