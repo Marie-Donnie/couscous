6012fde990efc9da1e7b0e67b681a3e02443b480
Licherm <montalvo@orange.fr>
2017-12-09 22:15:04 +0100

    Création clase Case

---
+++ src/couscous/connectors/Case.java
@@ -0,0 +1,47 @@
+package couscous.connectors;
+
+import java.util.ArrayList;
+
+public class Case {
+	
+	private ArrayList<String> from = new ArrayList<String>();
+	private ArrayList<String> to = new ArrayList<String> ();
+	
+	
+	// Add un couple dans from et to
+	public void addCouple(String f, String t) {
+		from.add(f);
+		from.add(t);
+	}
+	
+	// Prend en paramètre un string de la liste from
+	// Renvoie le string de la liste to au même index
+	//Pré-conditon : f apartient à la liste from
+	public String getToCorespondant(String f) {
+		String res ="";
+		int index = 0;
+		for (String e : from) {
+			if(e==f) {
+				index =from.indexOf(e);
+				res = to.get(index);
+			}
+		}
+		return res;
+	}
+	
+	public ArrayList<String> getFrom() {
+		return from;
+	}
+	public void setFrom(ArrayList<String> from) {
+		this.from = from;
+	}
+	public ArrayList<String> getTo() {
+		return to;
+	}
+	public void setTo(ArrayList<String> to) {
+		this.to = to;
+	}
+	
+	
+
+}
