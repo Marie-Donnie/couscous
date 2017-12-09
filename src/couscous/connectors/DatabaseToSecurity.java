ea90d64122fc2f4560fe0157b1d6fd99e47d3c9f
Licherm <montalvo@orange.fr>
2017-12-10 00:34:17 +0100

    Connector Data / Sécu

---
+++ src/couscous/connectors/DatabaseToSecurity.java
@@ -0,0 +1,15 @@
+package couscous.connectors;
+
+import couscous.observable.AbstractConnector;
+
+//Connector entre la BD et le géstionnaire de sécurité
+public class DatabaseToSecurity extends AbstractConnector {
+	
+	// Rapelle à l'inverse des Composants les fournis sont à gauche et les requis à droite
+	public DatabaseToSecurity () {
+		super();
+		this.addManageable("PortFRequestOpening", "PortRAskedOpening"); // Voir M1
+		this.addManageable("PortSendRequest", "PortRDataReady");
+	}
+
+}
