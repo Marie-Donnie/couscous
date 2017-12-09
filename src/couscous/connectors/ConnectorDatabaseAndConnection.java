58df179b555c8fb70de76a4cb4d9492dfe84c12b
Licherm <montalvo@orange.fr>
2017-12-10 00:49:03 +0100

    Refractor

--- src/couscous/connectors/ConnectorDatabaseAndConnection.java
+++ src/couscous/connectors/ConnectorDatabaseAndConnection.java
@@ -2,11 +2,11 @@ package couscous.connectors;
 
 import couscous.observable.AbstractConnector;
 
-public class DatabaseToConnection extends AbstractConnector {
+public class ConnectorDatabaseAndConnection extends AbstractConnector {
 	
 
 	// Rapelle à l'inverse des Composants les fournis sont à gauche et les requis à droite
-	public DatabaseToConnection() {
+	public ConnectorDatabaseAndConnection() {
 		super();
 		this.addManageable("PortFAskData", "PortRAskedData"); // Voir M1
 		this.addManageable("PortFSendData", "PortRReceiveData");
