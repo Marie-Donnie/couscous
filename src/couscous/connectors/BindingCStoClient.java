7c0251f538282b23417d1d2c7bf0e75a4e11d22b
Licherm <montalvo@orange.fr>
2017-12-10 00:11:33 +0100

    Routage first biding

--- src/couscous/connectors/BindingCStoClient.java
+++ src/couscous/connectors/BindingCStoClient.java
@@ -5,19 +5,12 @@ import java.util.ArrayList;
 import couscous.observable.AbstractConnector;
 
 public class BindingCStoClient extends AbstractConnector {
-	private ArrayList<String> manageable = new ArrayList<String>(); // Lié ou non des ports requis
 
-	@Override
-	public boolean ICanManageThis(String str) {
-		boolean verdict = false;
-		for ( String e : manageable) {
-			if(e == str) {
-				verdict = true;
-			}
-		}
-		return verdict;
+	public BindingCStoClient() {
+		super();
+		this.addManageable("PortAskClient", "PortReceiveAsk"); // Voir M1
 	}
-	
+
 
 	
 
