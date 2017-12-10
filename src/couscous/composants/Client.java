fdafd202eea6f56bff6b047898bbdfba999322ef
Marie-Donnie <delavergnemarie@gmail.com>
2017-12-10 15:35:55 +0100

    Ajouts de fonctions pour le client

--- src/couscous/composants/Client.java
+++ src/couscous/composants/Client.java
@@ -1,5 +1,9 @@
 package couscous.composants;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import couscous.message.Message;
 import couscous.observable.AbstractComposant;
 
@@ -20,6 +24,30 @@ public class Client extends AbstractComposant {
 		}else {
 			envoyerMessage(msg);
 		}	
+	}
+	
+	public void afficherReponse(String msg) {
+		System.out.println("La donnée que vous cherchiez est : " + msg);
+	}
+	
+	public Message demanderDonnee() {
+		String donneeCherchee = "";
+		String mdp = "";
+		System.out.println("Que voulez vous récupérer ? Vous pouvez chercher par nom ou id.");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+			donneeCherchee = br.readLine();
+		} catch (IOException e) {
+			System.out.println("Mauvais format.");
+			e.printStackTrace();
+		}
+        System.out.print("Mot de passe ?");
+        try{
+            mdp = br.readLine();
+        }catch(IOException e){
+            System.out.println("Mauvais format.");
+        }
+        return new Message("Initial", donneeCherchee , mdp);
 	}
 
 }
