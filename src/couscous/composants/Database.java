2d4e1f3aa59617107f266230532c8a0941617f3e
Marie-Donnie <delavergnemarie@gmail.com>
2017-12-10 14:49:59 +0100

    Ajout d'un commentaire

--- src/couscous/composants/Database.java
+++ src/couscous/composants/Database.java
@@ -23,6 +23,7 @@ public class Database extends AbstractComposant  {
 	@Override
 	public void recevoirMessage(Message msg) {
 		String message = msg.getMessage();
+		// Regarde si le message demande une ouverture ou les données
 		if (msg.getDestinataire().equals("PortRAskedOpening")) {
 			int ret = askedOpening(message);
 			if (ret != -1) {
